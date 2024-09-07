e8479e60d57f9c37d1e84c5cbe7a6bd536213131
Vitaliy <vi19954@gmail.com>
2024-09-08 01:06:23 +0300

    feat: change index slider

--- shelter/index.js
+++ shelter/index.js
@@ -9,27 +9,15 @@ document.addEventListener("DOMContentLoaded", function() {
     let body = document.getElementById('html')
     let arrow_previous = document.getElementById('button-arrow-previous')
     let arrrow_next = document.getElementById('button-arrow-next')
-    let cardSetPrevious = new Set()
-    let cardSetNext = new Set()
-    let cardSet = new Set()
     let setJson = new Set()
-    let prevFlag = false
-    let nextFlag = false
     let cardsSlider =  document.getElementById('cards-slider-ul')
-
-                let list = document.getElementById('cards-slider-ul')
-                let width = 270
-                let count = 3
-                let position = 0
-                let listElems = document.getElementById('cards-slider-ul').querySelectorAll('li')
+    let cardsArray = new Array()
 
     fetch('https://rolling-scopes-school.github.io/novogran-JSFEPRESCHOOL2024Q2/shelter/pets.json')
         .then(response => response.json())
         .then(json => {
 
             
-
-
             if(window.matchMedia("(max-width: 562px)").matches) {
                 cardConstructor(1)
             } else {
@@ -42,66 +30,145 @@ document.addEventListener("DOMContentLoaded", function() {
 
             window.matchMedia('(max-width: 904px)').addEventListener('change', (e) => {
                 if(!e.matches){
-                    cardSet.clear()
-                    clearCards()
                     cardConstructor(3)
                 } else {
-                    cardSet.clear()
-                    clearCards()
                     cardConstructor(2)
                 }
             })
 
             window.matchMedia('(max-width: 562px)').addEventListener('change', (e) => {
                 if(e.matches){
-                    cardSet.clear()
-                    clearCards()
                     cardConstructor(1)
                 } else {
-                    cardSet.clear()
-                    clearCards()
                     cardConstructor(2)
                 }
             })
-
-            function clearCards(){
-                for(let i = cardsSlider.children.length; i > 0 ; i--){
-                    cardsSlider.children[0].remove()
-                } 
-            }
             
             function cardConstructor(cardCount){
                 if(setJson.size == 0) setJson = new Set(json)
-                // if(prevFlag){
-                //     cardSet = new Set(cardSetPrevious)
-                //     prevFlag = !prevFlag
-                // } else {
-                //     if(nextFlag) {
-                //         cardSet = new Set(cardSetNext)
-                //         nextFlag = !nextFlag
-                //     }
-                // }
 
-                while(cardSet.size < cardCount){
-                    cardSet.add(Array.from(setJson)[(Math.floor(Math.random() * Array.from(setJson).length))])
+                for(let i = 0; i<3; i++){
+                    let cardSet = new Set()
+                    while(cardSet.size < cardCount){
+                        cardSet.add(Array.from(setJson)[(Math.floor(Math.random() * Array.from(setJson).length))])
+                    }
+                    cardsArray.push(cardSet)
                 }
                 
-                for(let object of cardSet){
+                for(let object of cardsArray){
+                    drawCard(object,'')
+                }
+                for(let card of document.getElementsByClassName('animalcard')){
+                    card.addEventListener('click', () => togglePopup(card.getElementsByTagName('h4')[0]))
+                }
+            }
+
+            let width = 1080
+            let cardsList = document.getElementById('cards-slider-ul')
+            let cardsCount = 3
+
+            if(window.matchMedia("(max-width: 581px)").matches) {
+                width = 270
+                cardsCount = 1
+            } else {
+                if(window.matchMedia("(max-width: 1279px)").matches) {
+                    width = 580
+                    cardsCount = 2
+                }
+            }
+
+            window.matchMedia('(max-width: 1279px)').addEventListener('change', (e) => {
+                if(!e.matches){
+                    width = 1080
+                    cardsCount = 3
+                } else {
+                    width = 580
+                    cardsCount = 2
+                }
+            })
+
+            window.matchMedia('(max-width: 581px)').addEventListener('change', (e) => {
+                if(e.matches){
+                    width = 270
+                    cardsCount = 1
+                } else {
+                    width = 580
+                    cardsCount = 2
+                }
+            })
+
+            arrrow_next.addEventListener('click', () => {
+
+                let cardSet = new Set()
+                while(cardSet.size < cardsCount){
+                    let randomAnimal = Array.from(setJson)[(Math.floor(Math.random() * Array.from(setJson).length))]
+                    if(!cardsArray[cardsArray.length-1].has(randomAnimal))
+                    cardSet.add(randomAnimal)
+                }
+                cardsArray.push(cardSet)
+                    drawCard(cardsArray[cardsArray.length-1], '')
+                    for(let i=0; i<cardsCount; i++){
+                        document.getElementById('cards-slider-ul').querySelector('li').remove()
+                    }
+
+                    cardsArray.shift()
+                    cardsList.style.transition = 'none'
+                    cardsList.style.marginLeft = 0 + 'px';
+
+                    setTimeout(() => {
+                        cardsList.style.transition = '1000ms'
+                        cardsList.style.marginLeft = -width + 'px'
+                      }, 100)
+            })
+
+            arrow_previous.addEventListener('click', () => {
+
+                let cardSet = new Set()
+                while(cardSet.size < cardsCount){
+                    let randomAnimal = Array.from(setJson)[(Math.floor(Math.random() * Array.from(setJson).length))]
+                    if(!cardsArray[0].has(randomAnimal))
+                    cardSet.add(randomAnimal)
+                }
+                cardsArray.unshift(cardSet)
+                drawCard(cardsArray[0],'left')
+
+                    for(let i=0; i<cardsCount; i++){
+                        let a = document.getElementById('cards-slider-ul').querySelectorAll('li')
+                        a[a.length-1].remove()
+                    }
+
+                    cardsArray.pop()
+                    cardsList.style.transition = 'none'
+                    cardsList.style.marginLeft = -width*2 + 'px';
+
+                    setTimeout(() => {
+                        cardsList.style.transition = '1000ms'
+                        cardsList.style.marginLeft = -width + 'px'
+                      }, 100)
+                    
+            })
+
+            function drawCard(cardsArray,direction){
+                for(let item of cardsArray){
                     let cardLi = document.createElement('li')
-                    document.getElementById('cards-slider-ul').append(cardLi)
+                    if(direction == 'left'){
+                        document.getElementById('cards-slider-ul').prepend(cardLi)
+                    } else {
+                        document.getElementById('cards-slider-ul').append(cardLi)
+                    }
                     let animalcard = document.createElement('div')
                     cardLi.append(animalcard)
                     animalcard.setAttribute('class', "animalcard")
                     animalcard.setAttribute('id', "animalcard")
                     let img = document.createElement('img')
-                    img.setAttribute('src', object.img)
+                    img.setAttribute('src', item.img)
                     img.setAttribute('alt', "animal")
                     img.setAttribute('id', "animal-img")
                     animalcard.appendChild(img)
                     let text = document.createElement('h4')
                     text.setAttribute('class', "animalcard-text")
                     text.setAttribute('id', "animalcard-text")
-                    text.innerHTML = object.name
+                    text.innerHTML = item.name
                     animalcard.appendChild(text)
                     let button = document.createElement('button')
                     button.setAttribute('class', "animalcard-button")
@@ -109,66 +176,7 @@ document.addEventListener("DOMContentLoaded", function() {
                     button.innerHTML = 'Learn more'
                     animalcard.appendChild(button)
                 }
-                for(let card of document.getElementsByClassName('animalcard')){
-                    card.addEventListener('click', () => togglePopup(card.getElementsByTagName('h4')[0]))
-                }
             }
-
-            arrow_previous.addEventListener('click', () => {
-                            
-                // position -= width * count
-                // position = Math.max(position, -width * (listElems.length - count))
-                // position = 50
-                // list.style.animation = 'scrolling-right 0.6s linear'
-
-                setJson = new Set(json)
-                setJson = new Set(setJson.difference(cardSet))
-                cardSetPrevious = new Set(cardSet)
-                prevFlag = true
-                nextFlag = false
-                cardSet.clear()
-                if(cardSetNext.size > 0) {
-                    if(cardSetNext.has(cardSet)) cardSet = new Set(cardSetNext)
-                }
-                // document.getElementById('cards-slider-ul').classList.add('animalcard-animation-right')
-                clearCards()
-                if(window.matchMedia("(max-width: 562px)").matches) {
-                    cardConstructor(1)
-                } else {
-                    if(window.matchMedia("(max-width: 904px)").matches) {
-                        cardConstructor(2)
-                    } else {cardConstructor(3)}
-                }
-            })
-
-            arrrow_next.addEventListener('click', () => {
-
-                // position += width * count
-                // position = Math.min(position, 0)
-                // position = -50
-                // list.style.animation = 'scrolling-left 0.6s linear'
-
-                setJson = new Set(json)
-                setJson = new Set(setJson.difference(cardSet))
-                cardSetNext = new Set(cardSet)
-                nextFlag = true
-                prevFlag = false
-                cardSet.clear()
-                if(cardSetPrevious.size > 0) {
-                    if(cardSetPrevious.has(cardSet)) cardSet = new Set(cardSetPrevious)
-                } 
-                
-                clearCards()
-                if(window.matchMedia("(max-width: 562px)").matches) {
-                    cardConstructor(1)
-                } else {
-                    if(window.matchMedia("(max-width: 904px)").matches) {
-                        cardConstructor(2)
-                    } else {cardConstructor(3)}
-                }
-                
-                // document.getElementById('cards-slider').classList.add('animalcard-animation-left')
-            })
         });
 
     
@@ -217,8 +225,6 @@ document.addEventListener("DOMContentLoaded", function() {
                     document.getElementById('popup-parasites').innerText = item.parasites
                 }
             }
-            // card.getElementById('name').innerHTML(car)
-            // document.getElementById('content').getElementsByTagName('img').setAttribute('src',card.)
         }
     }
 });
