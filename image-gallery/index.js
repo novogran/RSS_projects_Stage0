692478dd7618d51e0d819fbce14b37f1f446a5d5
Vitaly <vi19954@gmail.com>
2024-09-26 22:33:34 +0300

    feat: add load img from api and drawing it

--- image-gallery/index.js
+++ image-gallery/index.js
@@ -1,8 +1,27 @@
-const url = "https://type.fit/api/quotes";
+document.addEventListener("DOMContentLoaded", function () {
+    let img_conteiner = document.getElementById("img-conteiner")
 
-async function getData() {
-    const res = await fetch(url);
+
+async function getData(search) {
+    const res = await fetch(urlConstructor(search));
     const data = await res.json();
-    console.log(data);
-  }
-  getData();
+    drawImg(data)
+}
+
+function urlConstructor(search){
+    const baseUrl = 'https://api.unsplash.com/search/photos?query='
+    const endindRequest = '&per_page=30&orientation=landscape&client_id=DdjB2SU1HU34sF3ZUWuyXPSVg6z0ZvtBJnACokmvvcE'
+    return (search == '')? baseUrl+'lakes'+endindRequest : baseUrl+search+endindRequest
+}
+
+function drawImg(data){
+        for(let item of data.results){
+            let img = document.createElement('img')
+            img.setAttribute('src', item.urls.small)
+            img.setAttribute('alt', 'img')
+            img_conteiner.append(img)
+        }
+    }
+
+getData('lakes');
+})
